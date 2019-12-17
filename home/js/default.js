7399028ecf034bede68d795a2721635a15ce2266
Cherry-pellets <50487156+Cherry-pellets@users.noreply.github.com>
2019-12-17 10:56:47 +0800

    初始化各个页面

---
+++ home/js/default.js
@@ -0,0 +1,24 @@
+window.onload=function () {
+    /*头部左边：配送地区 移入-移出 事件*/
+    var area=document.getElementsByClassName("tools_area")[0];
+    var areaList=document.getElementsByClassName("areaList")[0];
+    area.onmouseenter=function () {
+        //alert("111");
+        areaList.classList.add("currentShow");
+    };
+    area.onmouseleave=function () {
+        areaList.classList.remove("currentShow");
+    }
+
+    /*点击某个城市*/
+    var city=Array.from(areaList.getElementsByTagName("li"));
+    var currentCity=document.getElementsByClassName("currentArea")[0];
+    city.forEach(ele=>{
+        ele.onclick = e=>{
+            let cityFont=ele.getElementsByTagName("a")[0].text;
+            currentCity.text=cityFont;
+            //console.log(cityFont);
+        }
+    });
+
+};
