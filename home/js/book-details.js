76e605aeed215ed32dd943ee88cf1dff8e1bbde7
于伟” <2235724297@qq.com>
2019-12-26 11:51:03 +0800

    增加了2个样式，图书排行榜的书被选中后添加阴影，图书详情页面的两个按钮被选中之后改变颜色，去掉了图书排行榜里面的一张图片

--- home/js/book-details.js
+++
@@ -1,4 +0,0 @@
-function onpicture(srcs) {
-    var img_large=document.getElementById("BigImg");
-    img_large.setAttribute("src",srcs);
-}
---
+++ home/js/book-details_bang.js
@@ -0,0 +1,41 @@
+function onpicture(srcs) {
+    var img_large=document.getElementById("BigImg");
+    img_large.setAttribute("src",srcs);
+}
+
+//商品被选中有阴影效果
+function selected(n){
+    var product="product_"+n;
+    var product_n=document.getElementById(product);
+    product_n.onmouseover=function show(){
+        product_n.style.backgroundColor="lightgray";
+    };
+    product_n.onmouseleave= function hide(){
+        product_n.style.backgroundColor="white";
+    };
+}
+
+
+function inita(a){
+    var shake=document.getElementById(a);
+    shake.onmouseover=function showa(){
+        shake.style.backgroundColor="#69bbd3";
+        shake.style.color="lightgray";
+    };
+    shake.onmouseleave=function backa(){
+        shake.style.backgroundColor="red";
+        shake.style.color="white";
+    };
+}
+
+function initb(b){
+    var shake=document.getElementById(b);
+    shake.onmouseover=function showb(){
+        shake.style.backgroundColor="#69bbd3";
+        shake.style.color="lightgray";
+    };
+    shake.onmouseleave=function backb(){
+        shake.style.backgroundColor="blue";
+        shake.style.color="white";
+    };
+}
